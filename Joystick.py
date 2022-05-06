c76b37d7b8540c0632c83a1b94841e3ce65d6dc8
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2022-05-06 08:47:14 +0200

    test with flask

--- Joystick.py
+++ Joystick.py
@@ -1,16 +1,43 @@
-import pyfirmata;
-import time;
+import pyfirmata
+import time
+import flask
+import flask_restful
+import requests
+from time import sleep 
+#import numpy
+app = flask.Flask("test")
 
-x = [1,2,3,4,5,6,7,8,9,1];
+api = flask_restful.Api(app)
+data = []
+class Data_Joystick:
+    def get(self):
+        return data
+    def post(self):
+        return data
 
-anchor = pyfirmata.Arduino()
-it =  pyfirmata.INPUT
+try:
+    board = pyfirmata.Arduino('COM4')
+    it = pyfirmata.util.Iterator(board)
+    it.start()
+    value_x = board.get_pin('a:0:i')
+    value_y = board.get_pin('a:1:i')
+    print(value_x.read())
+    print(value_y.read())
+    while True:
+        print("x :")
+        print(value_x.read())
+        print("y: ")
+        print(value_y.read())
+        time.sleep(0.32)  
+    #with open('log.txt', 'a') as f:
+        #f.write('X:'+ value_x.read() +'Y:' + value_y.read() + '\n');
+except:
+    print("Could't open the port")
+    print("Try to plug in your arduino board and try it")
+    print("In case it doesn't work even when you have plugged in your board\n try to change the port on the code")
 
-def write() :
-  time.wait(25)
-  while True:
-    x = anchor.board[10].input()
-    write(x)   
-
-y = anchor.board[11].input()
-    write
+api.add_resource(Data_Joystick)
+#url = "http://127.0.0.1:5000"
+#response = requests.get(url=url)
+#print(response.status_code)
+#print(response.text)    
--- joystick.py
+++ joystick.py
@@ -1,26 +1,43 @@
 import pyfirmata
 import time
+import flask
+import flask_restful
+import requests
 from time import sleep 
 #import numpy
-board = pyfirmata.Arduino('COM4')
-it = pyfirmata.util.Iterator(board)
-it.start()
-value_x = board.get_pin('a:0:i')
-value_y = board.get_pin('a:1:i')
-print(value_x.read())
-print(value_y.read())
-while True:
-    print("x :")
+app = flask.Flask("test")
+
+api = flask_restful.Api(app)
+data = []
+class Data_Joystick:
+    def get(self):
+        return data
+    def post(self):
+        return data
+
+try:
+    board = pyfirmata.Arduino('COM4')
+    it = pyfirmata.util.Iterator(board)
+    it.start()
+    value_x = board.get_pin('a:0:i')
+    value_y = board.get_pin('a:1:i')
     print(value_x.read())
-    print("y: ")
     print(value_y.read())
-    time.sleep(1.0);
-    
+    while True:
+        print("x :")
+        print(value_x.read())
+        print("y: ")
+        print(value_y.read())
+        time.sleep(0.32)  
     #with open('log.txt', 'a') as f:
         #f.write('X:'+ value_x.read() +'Y:' + value_y.read() + '\n');
-    
-    #x.digital[13].read()
-    #time.sleep(1)
-    #number = x.digital[14].read()
-    #time.wait(10)
-    #sleep(10)
+except:
+    print("Could't open the port")
+    print("Try to plug in your arduino board and try it")
+    print("In case it doesn't work even when you have plugged in your board\n try to change the port on the code")
+
+api.add_resource(Data_Joystick)
+#url = "http://127.0.0.1:5000"
+#response = requests.get(url=url)
+#print(response.status_code)
+#print(response.text)    
