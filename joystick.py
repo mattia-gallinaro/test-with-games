7e92aa1236b0d19e1cefcca34fb4b9c0bc89fad1
mattia-gallinaro <gallinaromattia6@gmail.com>
2022-05-12 09:43:12 +0200

    Test in linux
    Python can reach the desired libraries but he can't load the specified arduino port with pyFirmata and serial

--- joystick.py
+++ joystick.py
@@ -1,42 +1,64 @@
+from curses import baudrate
+from socket import timeout
 import pyfirmata
+import serial
+import importlib.util
 import time
 import flask
-import flask_restful
+import serial
+#import flask_restful
 import requests
+import sys
 from time import sleep 
+spec = importlib.util.spec_from_file_location("pyfirmata", "/home/mattia/.local/lib/python3.9/site-packages/pyfirmata/__init__.py")
+foo = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(foo)
+spec2 = importlib.util.spec_from_file_location("pyfirmata", serial.__file__)
+fo2 = importlib.util.module_from_spec(spec2)
+spec.loader.exec_module(fo2)
+#foo.()
+print(sys.path)
+print(pyfirmata.__file__)
 #import numpy
 app = flask.Flask("test")
 
-api = flask_restful.Api(app)
+#api = flask_restful.Api(app)
 data = []
 class Data_Joystick:
+    try:
+        board = fo2.Serial(port = "/dev/ttyACM0", baudrate = 9600, timeout = 0, rtscts =True)
+        it = pyfirmata.util.Iterator(board)
+        it.start()
+        value_x = board.get_pin('a:0:i')
+        value_y = board.get_pin('a:1:i')
+        print(value_x.read())
+        print(value_y.read())
+        while True:
+            print("x :")
+            print(value_x.read())
+            print("y: ")
+            print(value_y.read())
+            time.sleep(0.32)  
+    #with open('log.txt', 'a') as f:
+        #f.write('X:'+ value_x.read() +'Y:' + value_y.read() + '\n');
+    except:
+        print("Could't open the port")
+        print("Try to plug in your arduino board and try it")
+        print("In case it doesn't work even when you have plugged in your board\n try to change the port on the code")
     def get(self):
         return data
     def post(self):
         return data
 
-try:
-    board = pyfirmata.Arduino('COM4')
-    it = pyfirmata.util.Iterator(board)
-    it.start()
-    value_x = board.get_pin('a:0:i')
-    value_y = board.get_pin('a:1:i')
-    print(value_x.read())
-    print(value_y.read())
-    while True:
-        print("x :")
-        print(value_x.read())
-        print("y: ")
-        print(value_y.read())
-        time.sleep(0.32)  
-    #with open('log.txt', 'a') as f:
-        #f.write('X:'+ value_x.read() +'Y:' + value_y.read() + '\n');
-except:
-    print("Could't open the port")
-    print("Try to plug in your arduino board and try it")
-    print("In case it doesn't work even when you have plugged in your board\n try to change the port on the code")
 
-api.add_resource(Data_Joystick)
+
+#app.add_resource(Data_Joystick)
+if __name__ == "__main__":
+    #app.flask.run()
+    print("I can start the program")
+#if _main_ =="_main_":
+#    app.run()
+
 #url = "http://127.0.0.1:5000"
 #response = requests.get(url=url)
 #print(response.status_code)
