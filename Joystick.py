d369f70eb54b8d04cd7bed7bd44d1acdca8195c2
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2022-05-14 10:40:13 +0200

    .

--- Joystick.py
+++ Joystick.py
@@ -10,23 +10,29 @@ import serial
 import requests
 import sys
 from time import sleep 
-spec = importlib.util.spec_from_file_location("pyfirmata", "/home/mattia/.local/lib/python3.9/site-packages/pyfirmata/__init__.py")
-foo = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(foo)
-spec2 = importlib.util.spec_from_file_location("pyfirmata", serial.__file__)
-fo2 = importlib.util.module_from_spec(spec2)
-spec.loader.exec_module(fo2)
+#spec = importlib.util.spec_from_file_location("pyfirmata", "/home/mattia/.local/lib/python3.9/site-packages/pyfirmata/__init__.py")
+#foo = importlib.util.module_from_spec(spec)
+#spec.loader.exec_module(foo)
+#spec2 = importlib.util.spec_from_file_location("pyfirmata", serial.__file__)
+#fo2 = importlib.util.module_from_spec(spec2)
+#spec.loader.exec_module(fo2)
 #foo.()
-print(sys.path)
+#print(sys.path)
 print(pyfirmata.__file__)
 #import numpy
-app = flask.Flask("test")
-
+app = flask.Flask(__name__)
+app.config = ['DEBUG']
 #api = flask_restful.Api(app)
 data = []
 class Data_Joystick:
-    try:
-        board = fo2.Serial(port = "/dev/ttyACM0", baudrate = 9600, timeout = 0, rtscts =True)
+    def get(self):
+        return data
+    def post(self):
+        return data
+   
+
+try:
+        board = pyfirmata.Arduino("/dev/ttyACM0")
         it = pyfirmata.util.Iterator(board)
         it.start()
         value_x = board.get_pin('a:0:i')
@@ -41,25 +47,20 @@ class Data_Joystick:
             time.sleep(0.32)  
     #with open('log.txt', 'a') as f:
         #f.write('X:'+ value_x.read() +'Y:' + value_y.read() + '\n');
-    except:
+except:
         print("Could't open the port")
         print("Try to plug in your arduino board and try it")
         print("In case it doesn't work even when you have plugged in your board\n try to change the port on the code")
-    def get(self):
-        return data
-    def post(self):
-        return data
-
-
 
 #app.add_resource(Data_Joystick)
 if __name__ == "__main__":
     #app.flask.run()
     print("I can start the program")
+    app.run()
 #if _main_ =="_main_":
 #    app.run()
 
 #url = "http://127.0.0.1:5000"
 #response = requests.get(url=url)
 #print(response.status_code)
-#print(response.text)    
+#print(response.text) 
